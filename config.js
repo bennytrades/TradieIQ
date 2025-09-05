1d1b19653057828ccd5ade1d3b65c9dc386d5ee9
Trime <benhinkley@protonmail.com>
2025-09-05 19:54:10 +0930

    config JSon update

---
+++ config.js
@@ -0,0 +1,15 @@
+// TradieIQ Firebase Configuration
+// Update this file with your Firebase project settings
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPMEbNHpOmVoTWBtVd8sjCw04NdaHZiWU",
+  authDomain: "tradieiq-ff7c7.firebaseapp.com",
+  projectId: "tradieiq-ff7c7",
+  storageBucket: "tradieiq-ff7c7.firebasestorage.app",
+  messagingSenderId: "1032911231447",
+  appId: "1:1032911231447:web:3ec2652be817c1c28cd392",
+  measurementId: "G-GR2GLP5FKG"
+};
+
+// Make config available globally
+window.firebaseConfig = firebaseConfig;
